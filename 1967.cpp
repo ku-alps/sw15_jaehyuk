43ca697cf6a2f00580c73ffbd14dd34f726a116e
Jaehyuk Yang <43870422+yangjae33@users.noreply.github.com>
2019-07-14 21:44:48 +0900

    todo

--- 1967.cpp
+++ 1967.cpp
@@ -1,7 +1,74 @@
-#include<iostream>
-#include<cmath>
+//1967 트리의 지름
+
+#include <iostream>
+#include <deque>
+#include <algorithm>
+using namespace std;
+int n;
+int visited[10000] = {0};
+int edge[10000][10000] = {0};
+//deque<int> radius[10000];
+int rad[10000][10000] = {0};
+
+int dfs(int node,int dist){
+    if(visited[node] == 1)
+        return dist;
+    int cnt = 0;
+    
+    visited[node] = 1;
+    
+    for(int i = 0; i<n; i++){
+        if(edge[node][i] > 0 && visited[i] <= 0){
+            rad[node][i] = dfs(i,edge[node][i]);
+        }
+    }
+   
+    int localmax = 0;
+    for(int i = 0; i<n; i++){
+        cnt += rad[node][i];
+    
+        if(localmax < rad[node][i])
+            localmax = rad[node][i];
+    }
+    if(cnt == 0)
+        return dist;
+    else
+        return dist+localmax;
+}
+
 int main(){
-    double n;
-    std::cin>>n;
-    std::cout<<1+ceil(sqrt((n-1.0)/3.0+0.25)-0.5)<<'\n';
+    cin>>n;
+    int p,c,w;
+    for(int i = 0; i<n-1; i++){
+        cin>>p>>c>>w;
+        edge[p-1][c-1] = w;
+        edge[c-1][p-1] = w;
+    }
+    dfs(0,0);
+    //radius.resize(n);
+    /*
+    for(int i = 0; i<n; i++)
+    {
+        for(int j = 0; j<n; j++)
+        {
+            cout<<edge[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    for(int i = 0; i<n; i++)
+    {
+        for(int j = 0; j<n; j++)
+        {
+            cout<<rad[i][j]<<" ";
+        }
+        cout<<endl;
+    }*/
+    int total = 0;
+    int localmax = 0;
+    int a ,b;
+
+    
+    
+    cout<<total<<endl;
 }
