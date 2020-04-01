5f62ada4d7d156a85f73ad7f0fe9367044f18d10
Jaehyuk Yang <43870422+yangjae33@users.noreply.github.com>
2020-04-01 21:19:56 +0900

    200401#3월4주차

--- 2170.cpp
+++ 2170.cpp
@@ -1,42 +1,59 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include<bits/stdc++.h>
 
 using namespace std;
 
+//vector<pair<int,int> > arr;
 pair<int,int> arr[1000001];
-int N;
+bool cmp(pair<int,int>&a,pair<int,int>&b){
+    if(a.first<b.first){
+        return true;
+    }
+    else if(a.first==b.first){
+        if(a.second>b.second){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    return false;
+}
 
 int main(){
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(0);
-
+    int N;
     cin>>N;
+    int start=INT_MIN,end=INT_MIN;
     int a,b;
     for(int i = 0; i<N; i++){
         cin>>a>>b;
         arr[i].first = a;
         arr[i].second = b;
     }
-    sort(arr,arr+N);
-    
+    sort(arr,arr+N,cmp);
     int len = 0;
-    int left = arr[0].first;
-    int right = arr[0].second;
-    len += right-left;
-    
-    for(int i = 1; i<N; i++){
-        if(arr[i].first>right){
-            left  = arr[i].first;
-            right = arr[i].second;
-            len+=  right-left;
-        }
-        else if(arr[i].first<=right && arr[i].first>=left){
-            if(arr[i].second>right){
-                len+= arr[i].second-right;
-                right = arr[i].second;
+    for(int i=0; i<N; i++){
+        if(start<arr[i].first){
+            if(end<arr[i].first){
+                len += end-start;
+                start = arr[i].first;
+                end = arr[i].second;
             }
+            else{
+                if(arr[i].second>end){
+                    end = arr[i].second;
+                }
+            }
+        }      
+        if(i == N-1){
+            len+=end-start;
         }
     }
-    cout<<len<<endl;
+    cout<<len;
+/*
+    for(int i = 0; i<N; i++){
+        cout<<arr[i].first<<","<<arr[i].second<<endl;
+    }
+*/
 }
