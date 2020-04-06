947496e2bba64def6b19dd62bbd075503fcfb3a6
Jaehyuk Yang <43870422+yangjae33@users.noreply.github.com>
2020-04-06 19:42:18 +0900

    200406

--- 1629.cpp
+++ 1629.cpp
@@ -1,15 +1,28 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-
-int main(){
-    long long A,B,C;
-    cin>>A>>B>>C;
-
-    long long temp = A;
-    
-    for(int i = 0; i<B; i++){
-        temp = (temp%C) * (temp%C);
+long long A,B,C;
+int calc(long long num,int cnt){
+    if(cnt == 1){
+        return num%C;
     }
-    cout<<temp%C;
+    if(cnt == 0){
+        return 1;
+    }
+    if(cnt %2 == 0){
+        num = calc(num,cnt/2)%C;
+        return (num*num)%C;
+    }
+    return (num*calc(num,cnt-1)%C)%C;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+	cin.tie(0);
+    cin>>A>>B>>C;
+    long long temp = A;
+    if(B == 1){
+        cout<<A%C;
+        return 0;
+    }
+    cout<<calc(A,B)%C;
 }
