0e44e6ac166af597cde1e8ce189ef370902dfd1a
Jaehyuk Yang <43870422+yangjae33@users.noreply.github.com>
2020-04-16 17:57:33 +0900

    200416

--- 17070.cpp
+++ 17070.cpp
@@ -1,7 +1,60 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+int N;
+vector<vector<int> > arr;
+int cases = 0;
+int dfs(int x,int y, int kind){
+    if(x == N-1 && y==N-1){
+        cases++;
+        return 0;
+    }
+    if(kind == 0){
+        
+        if(y+1<=N-1 && arr[x][y+1] == 0){
+            dfs(x,y+1,0);
+        }
+        if((x+1<=N-1&&y+1<=N-1)&&arr[x+1][y+1] == 0){
+            if(arr[x+1][y]==0 && arr[x][y+1] == 0)
+                dfs(x+1,y+1,1);
+        }
+    }
+    else if(kind == 1){
+        if((x+1<=N-1&&y+1<=N-1)&&arr[x+1][y+1] == 0){
+            if(arr[x+1][y]==0 && arr[x][y+1] == 0)
+                dfs(x+1,y+1,1);
+        }
+        if(y+1<=N-1 && arr[x][y+1] == 0){
+            dfs(x,y+1,0);
+        }
+        if(x+1<=N-1 && arr[x+1][y] == 0){
+            dfs(x+1,y,2);
+        }
+    }
+    else{
+        if(x+1<=N-1 && arr[x+1][y] == 0){
+            dfs(x+1,y,2);
+        }
+        if((x+1<=N-1&&y+1<=N-1)&&arr[x+1][y+1] == 0){
+            if(arr[x+1][y]==0 && arr[x][y+1] == 0)
+                dfs(x+1,y+1,1);
+        }
+    }
+    return 0;
+}
 
 int main(){
     //Brute Force
+
+    cin>>N;
+    arr.resize(N);
+    for(int i = 0; i<N; i++)
+        arr[i].resize(N);
+    for(int i = 0; i<N; i++){
+        for(int j = 0; j<N; j++){
+            cin>>arr[i][j];
+        }
+    }
+    dfs(0,1,0);
+    cout<<cases<<endl;
 }
