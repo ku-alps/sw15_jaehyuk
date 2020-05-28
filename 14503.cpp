2772d6f7635b923551ae94b743fed56ceda5b239
Jaehyuk Yang <43870422+yangjae33@users.noreply.github.com>
2020-05-28 17:20:52 +0900

    200528

--- 14503.cpp
+++ 14503.cpp
@@ -1,172 +1,36 @@
-#include <iostream>
-#include <vector>
-
+#include<iostream>
 using namespace std;
-
-vector<vector<int> > arr;
-vector<vector<int> > visited;
-int main()
-{
-    int N, M;
-    cin >> N >> M;
-    int r, c, d;
-    cin >> r >> c >> d;
-
-    arr.resize(N);
-    visited.resize(N);
-    for (int i = 0; i < N; i++)
-    {
-        visited[i].resize(M);
-        fill(visited[i].begin(), visited[i].end(), 0);
-        arr[i].resize(M);
+int N,M;
+int dir[4][2] = {{-1,0},{0,1},{1,0},{0,-1}};
+int board[50][50];
+int visited[50][50] = {0};
+int main(){
+  cin>>N>>M;
+  int r,c,d;
+  for(int i = 0; i<N; i++){
+    for(int j = 0; j<M; j++){
+      cin>>board[i][j];
     }
-
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < M; j++)
-        {
-            cin >> arr[i][j];
-            if (arr[i][j] == 1)
-                visited[i][j] = 1;
-        }
+  }
+  int x = r;
+  int y = c;
+  visited[r][c] = 1;
+  while(true){
+    x = r;
+    y = c;
+    visited[r][c] = 1;
+    d = (4-(d+1))%4; 
+    x += dir[d][0];
+    y += dir[d][1];  
+    if(x>N-1||x<0 || y>M-1|| y<0){
+      continue;
     }
-    int stuck = 0;
-    int res = 0;
-    int k = 0;
-    while (true)
-    {
-        k++;
-        if(arr[r][c] == 0){
-            arr[r][c] = 2;
-            visited[r][c] = 1;
-            res ++;
-        }
-        if (d == 0)
-        {
-            if (c - 1 >= 0 && arr[r][c - 1] == 0)
-            {
-                c = c - 1;
-                stuck = 0;
-                d = 3;
-            }
-            else
-            {
-                d = 3;
-                stuck++;
-            }
-        }
-        else if (d == 1 && arr[r - 1][c] == 0)
-        {
-            if (r - 1 >= 0)
-            {
-                
-                r = r - 1;
-                stuck = 0;
-                d--;
-            }
-            else
-            {
-                d--;
-                stuck++;
-            }
-        }
-        else if (d == 2 && arr[r][c + 1] ==0)
-        {
-            if (c + 1 <= M - 1)
-            {
-                
-                c = c + 1;
-                stuck = 0;
-                d--;
-            }
-            else
-            {
-                d--;
-                stuck++;
-            }
-        }
-        else if (d == 3 && arr[r + 1][c] == 0)
-        {
-            if (r + 1 <= N - 1)
-            {
-                
-                r = r + 1;
-                stuck = 0;
-                d--;
-            }
-            else
-            {
-                d--;
-                stuck++;
-            }
-        }
-        if (((r + 1 < N && visited[r + 1][c] == 1) || r + 1 == N) 
-        && ((c + 1 < M && visited[r][c + 1] == 1) || c + 1 == M) 
-        && ((r - 1 >= 0 && visited[r - 1][c] == 1) || r - 1 == -1) 
-        && ((c - 1 >= 0 && visited[r][c - 1] == 1) || c - 1 == -1))
-        {
-            if (d == 0)
-            {
-                if (r + 1 < N && arr[r + 1][c] == 1)
-                {
-                    break;
-                }
-                else if (r + 1 == N)
-                {
-                    break;
-                }
-                else
-                {
-                    r++;
-                }
-            }
-            else if (d == 1)
-            {
-                if (c - 1 >= 0 && arr[r][c - 1] == 1)
-                {
-                    break;
-                }
-                else if (c - 1 == -1)
-                {
-                    break;
-                }
-                else
-                {
-                    c--;
-                }
-            }
-            else if (d == 2)
-            {
-                if (r - 1 >= 0 && arr[r - 1][c] == 1)
-                {
-                    break;
-                }
-                else if (r - 1 == -1)
-                {
-                    break;
-                }
-                else
-                {
-                    r--;
-                }
-            }
-            else
-            {
-                if (c + 1 < M && arr[r][c + 1] == 1)
-                {
-                    break;
-                }
-                else if (c + 1 == M)
-                {
-                    break;
-                }
-                else
-                {
-                    c++;
-                }
-            }
-        }
-        cout<<r<<" "<<c<<'\n';
+    if(board[x][y] == 1 && visited[x][y] == 1){
+      continue;
     }
-    cout<<res<<endl;
+    if(visited[x][y] == 0 && board[x][y] == 0){
+      r = x;
+      c = y;
+    }
+  }
 }
